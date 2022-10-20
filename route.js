15d32a01cf86c87bd0b6c02ffa7cbc3bfc33ac05
James Koay <88022942+james-koay@users.noreply.github.com>
2022-10-21 10:32:12 +1300

    added comment functionality

--- route.js
+++ route.js
@@ -6,59 +6,58 @@ const { log } = require('node:console')
 const router = express.Router()
 
 const fsPromises = require('node:fs/promises')
-
-
-
+const { stringify } = require('node:querystring')
 
 router.get('/:id', (req, res) => {
+  fsPromises
+    .readFile('./data.json', 'utf-8')
 
-  fsPromises.readFile('./data.json','utf-8')
+    .then((a) => {
+      const id = req.params.id
+      let artvariable = JSON.parse(a).horrorMovies.find((pic) => pic.id === +id)
 
-  .then( a => {
-
-     const id = req.params.id
-    let artvariable = JSON.parse(a).horrorMovies.find(pic =>  pic.id === +id)
-
-    res.render('details', artvariable)
-  
-  
-  })
-  .catch()
-
+      res.render('details', artvariable)
+    })
+    .catch()
 })
-
 
 router.get('/:id/edit', (req, res) => {
+  fsPromises
+    .readFile('./data.json', 'utf-8')
 
-  fsPromises.readFile('./data.json','utf-8')
+    .then((a) => {
+      const id = req.params.id
+      let artvariable = JSON.parse(a).horrorMovies.find((pic) => pic.id === +id)
 
-  .then( a => {
-
-     const id = req.params.id
-    let artvariable = JSON.parse(a).horrorMovies.find(pic =>  pic.id === +id)
-
-    res.render('edit', artvariable)
-  
-  })
-  .catch()
+      res.render('edit', artvariable)
+    })
+    .catch()
 })
-
 
 router.post('/:id/edit', (req, res) => {
+  fsPromises
+    .readFile('./data.json', 'utf-8')
 
-  fsPromises.readFile('./data.json','utf-8')
+    .then((dataset) => {
+      const id = req.params.id
+      const newComments = req.body.comments
+      const movieData = JSON.parse(dataset)
+      let movieObject = movieData.horrorMovies.find((pic) => pic.id == id)
 
-  .then( a => {
+      const index = movieData.horrorMovies.indexOf(movieObject)
+      movieObject.comments.push(newComments)
 
-     const id = req.params.id
-     
-    let artvariable = JSON.parse(a).horrorMovies.find(pic =>  pic.id === +id)
+      movieData.horrorMovies.splice(index, 1, movieObject)
 
-    res.render('details', artvariable)
-  
-  })
-  .catch()
+      return fsPromises.writeFile(
+        './data.json',
+        JSON.stringify(movieData, null, 2)
+      )
+    })
+    .then(() => {
+      res.redirect('/movie/' + req.params.id)
+    })
+    .catch()
 })
-
 
 module.exports = router
